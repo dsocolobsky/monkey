a0af6fbff6a11c2f754be11c380625900b60d43e
Dylan Socolobsky <dsocolobsky@github.com>
2020-07-04 19:06:05 -0300

    expression statement struct (p50)

--- ast/ast.go
+++ ast/ast.go
@@ -59,3 +59,13 @@ func (rs *ReturnStatement) statementNode() {}
 func (rs *ReturnStatement) TokenLiteral() string {
 	return rs.Token.Literal
 }
+
+type ExpressionStatement struct {
+	Token      token.Token // first token of the expression
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+func (es *ExpressionStatement) TokenLiteral() string {
+	return es.Token.Literal
+}
