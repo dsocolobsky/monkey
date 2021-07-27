362979c99dadf665b2f90ee062897587c75b01ac
Dylan Socolobsky <dsocolobsky@gmail.com>
2021-07-27 18:16:56 -0300

    Beggining of Evaluator

---
+++ evaluator/evaluator.go
@@ -0,0 +1,28 @@
+package evaluator
+
+import (
+	"github.com/dsocolobsky/monkey/ast"
+	"github.com/dsocolobsky/monkey/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	}
+	return nil
+}
+
+func evalStatements(statements []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, statement := range statements {
+		result = Eval(statement)
+	}
+
+	return result
+}
