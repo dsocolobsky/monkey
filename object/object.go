926b2161aa79061b3afb32360dc517675a60678e
Dylan Socolobsky <dsocolobsky@gmail.com>
2021-07-27 18:04:21 -0300

    Boolean Object

--- object/object.go
+++ object/object.go
@@ -6,6 +6,7 @@ type ObjectType string
 
 const (
 	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
 )
 
 type Object interface {
@@ -23,4 +24,16 @@ func (i Integer) Type() ObjectType {
 
 func (i Integer) Inspect() string {
 	return fmt.Sprintf("%d", i.Value)
+}
+
+type Boolean struct {
+	Value bool
+}
+
+func (b Boolean) Type() ObjectType {
+	return BOOLEAN_OBJ
+}
+
+func (b Boolean) Inspect() string {
+	return fmt.Sprintf("%t", b.Value)
 }
