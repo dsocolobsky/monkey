a6d150d20bd3e935bd639fda5ab5255d2507f918
Dylan Socolobsky <dsocolobsky@gmail.com>
2021-07-27 19:34:21 -0300

    Eval minus prefix operator (p 119)

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -33,6 +33,8 @@ func evalPrefixExpression(operator string, right object.Object) object.Object {
 	switch operator {
 	case "!":
 		return evalBangOperatorExpression(right)
+	case "-":
+		return evalMinusOperatorExpression(right)
 	default:
 		return NULL
 	}
@@ -49,6 +51,15 @@ func evalBangOperatorExpression(right object.Object) object.Object {
 	default:
 		return FALSE
 	}
+}
+
+func evalMinusOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return NULL
+	}
+
+	value := right.(*object.Integer).Value
+	return &object.Integer{Value: -value}
 }
 
 func evalStatements(statements []ast.Statement) object.Object {
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -15,6 +15,10 @@ func TestEvalIntegerExpression(t *testing.T) {
 	}{
 		{"5", 5},
 		{"10", 10},
+		{"-5", -5},
+		{"-10", -10},
+		{"--5", 5},
+		{"---5", -5},
 	}
 
 	for _, tt := range tests {
