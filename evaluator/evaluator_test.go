4ae75a3c6e364230a8ab60ad4488bb1106235cec
Dylan Socolobsky <dsocolobsky@gmail.com>
2021-07-27 18:38:20 -0300

    Test Boolean evaluation

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -13,7 +13,7 @@ func TestEvalIntegerExpression(t *testing.T) {
 		input    string
 		expected int64
 	}{
-		{"5", 2},
+		{"5", 5},
 		{"10", 10},
 	}
 
@@ -23,17 +23,39 @@ func TestEvalIntegerExpression(t *testing.T) {
 	}
 }
 
+func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
+	result, ok := obj.(*object.Integer)
+	assert.True(t, ok)
+	assert.Equal(t, result.Value, expected)
+	return true
+}
+
+func TestEvalBooleanExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"true", true},
+		{"false", false},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
+}
+
+func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
+	result, ok := obj.(*object.Boolean)
+	assert.True(t, ok)
+	assert.Equal(t, result.Value, expected)
+	return true
+}
+
 func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
 	program := p.ParseProgram()
 
 	return Eval(program)
-}
-
-func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
-	result, ok := obj.(*object.Integer)
-	assert.True(t, ok)
-	assert.Equal(t, result.Value, expected)
-	return true
 }
